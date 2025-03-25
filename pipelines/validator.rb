3602218ba7245f6155152b958b4383d64ab93e3d
nicolas <nicordenes@gmail.com>
2025-03-25 00:02:29 -0300

    feat: add rules to Validator pipeline

--- pipelines/validator.rb
+++ pipelines/validator.rb
@@ -5,6 +5,18 @@ class Validator < Tanakai::Pipeline
     # No duplicates
     raise DropItemError, "Item url is not unique" unless unique?(:url, item[:url])
 
+    # No out-of-stock items
+    raise DropItemError, "Item out of stock" unless item[:stock]
+
+    # Has title present
+    raise DropItemError, "Invalid title" unless item[:title].present?
+
+    # Catch zero prices
+    raise DropItemError, "Invalid price" if item[:price].zero?
+
+    # Catch relative urls
+    raise DropItemError, "Invalid url" if URI.parse(item[:url]).host.nil?
+
     # Pass item to the next pipeline (if it wasn't dropped)
     item
   end
