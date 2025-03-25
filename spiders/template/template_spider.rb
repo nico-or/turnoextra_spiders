8d9b3b583cada14d854f184a017e273bc0a0f85c
nicolas <nicordenes@gmail.com>
2025-03-25 00:40:27 -0300

    add template spider

---
+++ spiders/template/template_spider.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+# Template store spider
+# Engine: ??
+class TemplateSpider < ApplicationSpider
+  @name = "template_spider"
+  @store = {
+    name: "template",
+    url: "https://www.template.cl/"
+  }
+  @start_urls = ["https://www.example.com/"]
+  @config = {}
+
+  def parse(response, url:, data: {})
+    parse_index(response)
+    paginate(response, url)
+  end
+
+  # Parse a Nokogiri::Element and call #parse_product_node on all elements
+  def parse_index(response, url: nil, data: {})
+    listings = response.css("")
+    listings.each { parse_product_node(it) }
+  end
+
+  # Parse a Nokogiri::Element representing a listing and call #send_item on it
+  def parse_product_node(node)
+    item = {}
+    item[:url] = node.at_css("")[:href]
+    item[:title] = node.at_css("").text
+    item[:price] = node.at_css("").text.then { scan_int(it) }
+    item[:stock] = true
+
+    send_item item
+  end
+
+  def paginate(response, url)
+    next_page = response.at_css("")
+    return unless next_page
+
+    request_to :parse, url: absolute_url(next_page[:href], base: url)
+  end
+end
