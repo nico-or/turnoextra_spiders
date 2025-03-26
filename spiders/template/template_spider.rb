3c718818b31f9347be60fe992aa400804e1412fb
nicolas <nicordenes@gmail.com>
2025-03-26 12:36:42 -0300

    refactor: update TemplateSpider to current interface

--- spiders/template/template_spider.rb
+++ spiders/template/template_spider.rb
@@ -27,16 +27,28 @@ class TemplateSpider < ApplicationSpider
     item = {}
     item[:url] = node.at_css("")[:href]
     item[:title] = node.at_css("").text
-    item[:price] = node.at_css("").text.then { scan_int(it) }
-    item[:stock] = true
+    item[:price] = get_price(node)
+    item[:stock] = in_stock(node)
 
     send_item item
   end
+
+  private
 
   def paginate(response, url)
     next_page = response.at_css("")
     return unless next_page
 
     request_to :parse, url: absolute_url(next_page[:href], base: url)
+  end
+
+  def get_price(node)
+    price_node = node.at_css("")
+    price_text = price_node.text
+    scan_int(price_text)
+  end
+
+  def in_stock?(_node)
+    true
   end
 end
