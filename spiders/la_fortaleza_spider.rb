706d6a07a3b870aa6aa9f5c28b6d0f05e7a862ce
nicolas <nicordenes@gmail.comm>
2025-06-07 18:12:25 -0400

    feat: add LaFortalezaSpider

---
+++ spiders/la_fortaleza_spider.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+# La Fortaleza store spider
+# engine: jumpseller
+class LaFortalezaSpider < ApplicationSpider
+  @name = "la_fortaleza_spider"
+  @store = {
+    name: "La Fortaleza",
+    url: "https://www.lafortalezapuq.cl"
+  }
+  @start_urls = ["https://www.lafortalezapuq.cl/jdm"]
+  @config = {}
+
+  def parse(response, url:, data: {})
+    parse_index(response, url:)
+    paginate(response, url:)
+  end
+
+  # Parse a Nokogiri::Element and call #parse_product_node on all elements
+  def parse_index(response, url:, data: {})
+    nodes = response.css("div.products figure.product")
+    nodes.each { |node| parse_product_node(node, url:) }
+  end
+
+  # Parse a Nokogiri::Element representing a listing and call #send_item on it
+  def parse_product_node(node, url:)
+    item = {}
+
+    item[:url] = get_url(node, url)
+    item[:title] = get_title(node)
+    item[:price] = get_price(node)
+    item[:stock] = in_stock?(node)
+    item[:image_url] = get_image_url(node)
+
+    send_item item
+  end
+
+  def paginate(response, url:)
+    next_page = response.at_css("nav.pagination-next-prev a[@class=next]")
+    return unless next_page
+
+    request_to :parse, url: absolute_url(next_page[:href], base: url)
+  end
+
+  def get_url(node, url)
+    rel_url = node.at_css("a")[:href]
+    absolute_url(rel_url, base: url)
+  end
+
+  def get_title(node)
+    node.at_css("h5").text
+  end
+
+  def get_price(node)
+    price_node = node.at_css("span.product-price-discount i")
+    scan_int(price_node.text)
+  end
+
+  def in_stock?(_node)
+    true
+  end
+
+  def get_image_url(node)
+    # example: https://cdnx.jumpseller.com/la-fortaleza-punta-arenas1/image/13909331/thumb/230/260?1610821805
+    node.at_css("img")[:src].split("/thumb").first
+  rescue NoMethodError
+    nil
+  end
+end
