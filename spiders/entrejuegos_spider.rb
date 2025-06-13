b1b2c33daaf0c70a2b49f73302a50006441e1dff
nicolas <nicordenes@gmail.comm>
2025-06-13 13:34:09 -0400

    refactor(EntrejuegosSpider): make more testable
    Remove side effects from parsing functions to facilitate tests.

--- spiders/entrejuegos_spider.rb
+++ spiders/entrejuegos_spider.rb
@@ -12,35 +12,39 @@ class EntrejuegosSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
   def parse_index(response, url: nil, data: {})
     listings = response.css("div#js-product-list article")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
   def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = in_stock?(node)
-    item[:image_url] = get_image_url(node)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: in_stock?(node),
+      image_url: get_image_url(node)
+    }
+  end
 
-    send_item item
+  def next_page_url(response, url)
+    next_page_node = response.at_css("nav.pagination li a[@rel=next]")
+    return unless next_page_node
+
+    absolute_url(next_page_node[:href], base: url)
   end
 
   private
 
   def paginate(response, url)
-    next_page = response.at_css("nav.pagination li a[@rel=next]")
-    return unless next_page
-
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
   end
 
   def get_url(node)
