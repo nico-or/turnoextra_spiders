62ee7372549de3bac35b81988295af4b1e32f0f9
nicolas <nicordenes@gmail.com>
2025-06-13 19:04:19 -0400

    refactor(UpdownSpider): update interface

--- spiders/updown_spider.rb
+++ spiders/updown_spider.rb
@@ -12,35 +12,39 @@ class UpdownSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("ul.products li.product")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
-  def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = true
-    item[:image_url] = get_image_url(node)
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
+  end
 
-    send_item item
+  def next_page_url(response, url)
+    next_page = response.at_css("nav.woocommerce-pagination li a.next")
+    return unless next_page
+
+    absolute_url(next_page[:href], base: url)
   end
 
   private
 
   def paginate(response, url)
-    next_page = response.at_css("nav.woocommerce-pagination li a.next")
-    return unless next_page
-
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
   end
 
   def get_url(node)
@@ -54,6 +58,10 @@ class UpdownSpider < ApplicationSpider
   def get_price(node)
     price_node = node.css("span.price bdi").last
     scan_int(price_node.text)
+  end
+
+  def purchasable?(_node)
+    true
   end
 
   def get_image_url(node)
