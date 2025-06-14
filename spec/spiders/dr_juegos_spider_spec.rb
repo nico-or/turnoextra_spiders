fd43ec40a8336dccd7ae20cd4b9def0a3b8e0934
nicolas <nicordenes@gmail.comm>
2025-06-13 20:19:09 -0400

    test(DrJuegosSpider): add tests

---
+++ spec/spiders/dr_juegos_spider_spec.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require "spider_helper"
+
+RSpec.describe DrJuegosSpider do
+  let(:spider) { described_class.new }
+  let(:store_url) { described_class.store[:url] }
+
+  it_behaves_like "a store spider"
+
+  describe "#parse_index" do
+    let(:response) do
+      html = File.read("spec/fixtures/dr_juegos/index_page_paginate_true.html")
+      Nokogiri::HTML(html)
+    end
+
+    it "returns 12 items" do
+      items = spider.send(:parse_index, response, url: store_url)
+      expect(items.length).to eq(12)
+    end
+  end
+
+  describe "#parse_index_node" do
+    context "with a regular index product node" do
+      let(:filename) { "spec/fixtures/dr_juegos/product_index_node_regular.html" }
+
+      let(:expected) do
+        {
+          url: "https://www.drjuegos.cl/todos-los-productos/2116-zombicide-white-dead.html",
+          title: "Zombicide - White Dead",
+          price: 110_000,
+          stock: true,
+          image_url: "https://www.drjuegos.cl/7839-large_default/zombicide-white-dead.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with a discounted price index product node" do
+      let(:filename) { "spec/fixtures/dr_juegos/product_index_node_discounted.html" }
+
+      let(:expected) do
+        {
+          url: "https://www.drjuegos.cl/todos-los-productos/1523-ierusalem.html",
+          title: "Ierusalem",
+          price: 45_000,
+          stock: true,
+          image_url: "https://www.drjuegos.cl/4624-large_default/ierusalem.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with an out-of-stock index product node" do
+      let(:filename) { "spec/fixtures/dr_juegos/product_index_node_out_of_stock.html" }
+
+      let(:expected) do
+        {
+          url: "https://www.drjuegos.cl/todos-los-productos/2081-mint-works.html",
+          title: "Mint Delivery",
+          price: 16_000,
+          stock: false,
+          image_url: "https://www.drjuegos.cl/7739-large_default/mint-works.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+  end
+
+  describe "#next_page_url" do
+    context "with an index page that has a next page link" do
+      let(:response) do
+        html = File.read("spec/fixtures/dr_juegos/index_page_paginate_true.html")
+        Nokogiri::HTML(html)
+      end
+
+      it "has a next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = "https://www.drjuegos.cl/2-todos-los-productos?page=2"
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context "with an index page that hasn't a next page link" do
+      let(:response) do
+        html = File.read("spec/fixtures/dr_juegos/index_page_paginate_false.html")
+        Nokogiri::HTML(html)
+      end
+
+      it "has no next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = nil
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+end
