81b3444761270a980a336aea3795c2f9e9dfeb93
nicolas <nicordenes@gmail.com>
2025-06-14 17:13:30 -0400

    refactor(VudugamingSpider): update interface

--- spiders/vudugaming_spider.rb
+++ spiders/vudugaming_spider.rb
@@ -12,35 +12,39 @@ class VudugamingSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response, url: url)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("article.product-block")
-    listings.each { parse_product_node(it, url) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
-  def parse_product_node(node, url)
-    item = {}
-    item[:url] = get_url(node, url)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = in_stock?(node)
-    item[:image_url] = get_image_url(node)
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node, url),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
+  end
 
-    send_item item
+  def next_page_url(response, url)
+    next_page = response.at_css("ul.pager li.next a")
+    return unless next_page
+
+    absolute_url(next_page[:href], base: url)
   end
 
   private
 
   def paginate(response, url)
-    next_page = response.at_css("ul.pager li.next a")
-    return unless next_page
-
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
   end
 
   def get_url(node, url)
@@ -57,7 +61,11 @@ class VudugamingSpider < ApplicationSpider
   end
 
   def in_stock?(node)
-    node.at_css(".product-block__actions form") != nil
+    !node.at_css(".product-block__actions form").nil?
+  end
+
+  def purchasable?(node)
+    in_stock?(node)
   end
 
   def get_image_url(node)
