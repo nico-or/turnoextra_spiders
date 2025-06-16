10d0ef0a1c36ebc67cee827181fe7934e351bec2
nicolas <nicordenes@gmail.comm>
2025-06-16 16:53:49 -0400

    fix(PlaycenterSpider): correct image_url parsing

--- spiders/playcenter_spider.rb
+++ spiders/playcenter_spider.rb
@@ -70,7 +70,12 @@ class PlaycenterSpider < ApplicationSpider
   end
 
   def get_image_url(node)
-    node.at_css("img")["srcset"].split(", ").last.split.first
+    # Example: https://playcenter.cl/wp-content/uploads/2025/05/D_NQ_NP_2X_755218-MLC80923289084_122024-F-300x300.webp
+    full_url = node.at_css("img")["src"]
+    match = full_url.match(/(?<url>.*)(?<size>-\d+x\d+)(?<ext>\..*)/)
+    return unless match
+
+    "#{match[:url]}#{match[:ext]}"
   rescue NoMethodError
     nil
   end
