ff16ecaf07c2d8446af33d122a0e363cb89c99d0
nicolas <nicordenes@gmail.com>
2025-06-16 19:07:54 -0400

    refactor(AldeaJuegosSpider): update interface

--- spiders/aldea_juegos_spider.rb
+++ spiders/aldea_juegos_spider.rb
@@ -12,36 +12,40 @@ class AldeaJuegosSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("div#js-product-list article")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
-  def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = true # Stock filtered by URL query parameter
-    item[:image_url] = get_image_url(node)
-
-    send_item item
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
   end
 
-  def paginate(response, url)
-    next_page = response.at_css("nav.pagination li a[@rel=next]")
-    return unless next_page
+  def next_page_url(response, url)
+    next_page_node = response.at_css("nav.pagination li a[@rel=next]")
+    return unless next_page_node
 
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    absolute_url(next_page_node[:href], base: url)
   end
 
   private
+
+  def paginate(response, url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
+  end
 
   def get_url(node)
     node.at_css(".product-title a")[:href]
@@ -55,6 +59,14 @@ class AldeaJuegosSpider < ApplicationSpider
   def get_price(node)
     price_node = node.css("span.price").last
     scan_int(price_node.text)
+  end
+
+  def in_stock?(_node)
+    true
+  end
+
+  def purchasable?(node)
+    in_stock?(node)
   end
 
   def get_image_url(node)
