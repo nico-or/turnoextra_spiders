368321e81a34f880daadc2c67f9a62da233637f3
nicolas <nicordenes@gmail.com>
2025-06-16 12:19:23 -0400

    refactor(DementegamesSpider): update interface

--- spiders/dementegames_spider.rb
+++ spiders/dementegames_spider.rb
@@ -12,33 +12,39 @@ class DementegamesSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("div#js-product-list article")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # parse a Nokogiri node and return an item
-  def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = true # Stock filtered by URL query parameter
-    item[:image_url] = get_image_url(node)
-
-    send_item item
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
   end
 
-  def paginate(response, url)
-    next_page = response.at_css("nav.pagination li a[@rel=next]")
+  def next_page_url(response, url)
+    next_page = response.at_css("nav.pagination li a[rel=next]")
     return unless next_page
 
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    absolute_url(next_page[:href], base: url)
+  end
+
+  private
+
+  def paginate(response, url)
+    next_url = next_page_url(response, url)
+    request_to(:parse, url: next_url) if next_url
   end
 
   def get_url(node)
@@ -51,6 +57,14 @@ class DementegamesSpider < ApplicationSpider
 
   def get_price(node)
     node.at_css("span.price")[:content].to_i
+  end
+
+  def in_stock?(_node)
+    true
+  end
+
+  def purchasable?(node)
+    in_stock?(node)
   end
 
   def get_image_url(node)
