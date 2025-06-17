f84ab987fd6450ee966d66cc6baf0e79c28f045d
nicolas <nicordenes@gmail.comm>
2025-06-17 16:38:32 -0400

    test(RevarukSpider): add tets

---
+++ spec/spiders/revaruk_spider_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require "spider_helper"
+
+RSpec.describe RevarukSpider do
+  let(:fixture_directory) { "revaruk" }
+  let(:spider) { described_class.new }
+  let(:store_url) { described_class.store[:url] }
+
+  it_behaves_like "a store spider"
+
+  describe "#parse_index" do
+    let(:response) do
+      html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+      Nokogiri::HTML(html)
+    end
+
+    it "returns 12 items" do
+      items = spider.send(:parse_index, response, url: store_url)
+      expect(items.length).to eq(12)
+    end
+  end
+
+  describe "#parse_index_node" do
+    context "with a regular index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_regular.html") }
+
+      let(:expected) do
+        {
+          url: "https://revaruk.cl/producto/telestrations-80s-y-90s-expansion/",
+          title: "Telestrations 80s y 90s (Expansión)",
+          price: 7_990,
+          stock: true,
+          image_url: "https://revaruk.cl/wp-content/uploads/telestrations-expansion.webp"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with a discounted price index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_discounted.html") }
+
+      let(:expected) do
+        {
+          url: "https://revaruk.cl/producto/piratas-de-maracaibo/",
+          title: "Piratas de Maracaibo",
+          price: 64_990,
+          stock: true,
+          image_url: "https://revaruk.cl/wp-content/uploads/piratas-de-maracaibo-_1_.webp"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with an out-of-stock index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_out_of_stock.html") }
+
+      let(:expected) do
+        {
+          url: "https://revaruk.cl/producto/guacamole/",
+          title: "Guacamole",
+          price: 15_990,
+          stock: false,
+          image_url: "https://revaruk.cl/wp-content/uploads/guacamole-juego-de-mesa.webp"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+  end
+
+  describe "#next_page_url" do
+    context "with an index page that has a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has a next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = "https://revaruk.cl/product-category/juegos-de-mesa/page/2/"
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context "with an index page that hasn't a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_false.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has no next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = nil
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+end
