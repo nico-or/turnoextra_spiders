1136fd3ce5ff6dd956a6c5cfb9b6958c81eef00b
nicolas <nicordenes@gmail.comm>
2025-06-17 17:58:06 -0400

    refactor(ElDadoSpider): update interface

--- spiders/el_dado_spider.rb
+++ spiders/el_dado_spider.rb
@@ -12,36 +12,40 @@ class ElDadoSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("div.product-grid-items div.product-type-simple")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
-  def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = in_stock?(node)
-    item[:image_url] = get_image_url(node)
-
-    send_item item
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
   end
 
-  def paginate(response, url)
-    next_page = response.at_css("nav.woocommerce-pagination a.next")
+  def next_page_url(response, url)
+    next_page = response.at_css("nav.woocommerce-pagination li a.next")
     return unless next_page
 
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    absolute_url(next_page[:href], base: url)
   end
 
   private
+
+  def paginate(response, url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
+  end
 
   def get_url(node)
     node.at_css("a")[:href]
@@ -51,15 +55,19 @@ class ElDadoSpider < ApplicationSpider
     node.at_css("h2").text.strip
   end
 
-  def in_stock?(node)
-    node.classes.include?("instock")
-  end
-
   def get_price(node)
     price_node = node.css("span.price bdi").last
     # somehow there are no prices on some items...
     # ex: https://eldado.cl/producto/dobble-31-minutos/ (10/05/2025)
     scan_int(price_node.text) if price_node
+  end
+
+  def in_stock?(node)
+    node.classes.include?("instock")
+  end
+
+  def purchasable?(node)
+    in_stock?(node)
   end
 
   def get_image_url(node)
