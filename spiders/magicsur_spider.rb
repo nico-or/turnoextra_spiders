ca979e7984763f0d296d7f4b95601c13896796bd
nicolas <nicordenes@gmail.comm>
2025-06-17 11:05:51 -0400

    refactor(MagicsurSpider): update interface

--- spiders/magicsur_spider.rb
+++ spiders/magicsur_spider.rb
@@ -12,33 +12,39 @@ class MagicsurSpider < ApplicationSpider
   @config = {}
 
   def parse(response, url:, data: {})
-    parse_index(response)
+    items = parse_index(response, url:)
+    items.each { |item| send_item item }
+
     paginate(response, url)
   end
 
-  # Parse a Nokogiri::Element and call #parse_product_node on all elements
-  def parse_index(response, url: nil, data: {})
+  def parse_index(response, url:, data: {})
     listings = response.css("div#js-product-list article")
-    listings.each { parse_product_node(it) }
+    listings.map { |listing| parse_product_node(listing, url:) }
   end
 
-  # Parse a Nokogiri::Element representing a listing and call #send_item on it
-  def parse_product_node(node)
-    item = {}
-    item[:url] = get_url(node)
-    item[:title] = get_title(node)
-    item[:price] = get_price(node)
-    item[:stock] = true
-    item[:image_url] = get_image_url(node)
-
-    send_item item
+  def parse_product_node(node, url:)
+    {
+      url: get_url(node),
+      title: get_title(node),
+      price: get_price(node),
+      stock: purchasable?(node),
+      image_url: get_image_url(node)
+    }
   end
+
+  def next_page_url(response, url)
+    next_page_node = response.at_css("nav.pagination li a[@rel=next]")
+    return unless next_page_node
+
+    absolute_url(next_page_node[:href], base: url)
+  end
+
+  private
 
   def paginate(response, url)
-    next_page = response.at_css("nav.pagination li a[@rel=next]")
-    return unless next_page
-
-    request_to :parse, url: absolute_url(next_page[:href], base: url)
+    next_page_url = next_page_url(response, url)
+    request_to(:parse, url: next_page_url) if next_page_url
   end
 
   def get_url(node)
@@ -46,12 +52,20 @@ class MagicsurSpider < ApplicationSpider
   end
 
   def get_title(node)
-    node.at_css("h2").text
+    node.at_css("h2").text.strip
   end
 
   def get_price(node)
     price_node = node.at_css("span.product-price")
     scan_int(price_node.text)
+  end
+
+  def in_stock?(_node)
+    true
+  end
+
+  def purchasable?(node)
+    in_stock?(node)
   end
 
   def get_image_url(node)
