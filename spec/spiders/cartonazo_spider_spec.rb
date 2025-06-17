0afdef97ff1b99c53f13cf8f5cf87b75dbb7937f
nicolas <nicordenes@gmail.comm>
2025-06-17 17:04:00 -0400

    test(CartonazoSpider): add tests

---
+++ spec/spiders/cartonazo_spider_spec.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "spider_helper"
+
+RSpec.describe CartonazoSpider do
+  let(:fixture_directory) { "cartonazo" }
+  let(:spider) { described_class.new }
+  let(:store_url) { described_class.store[:url] }
+
+  it_behaves_like "a store spider"
+
+  describe "#parse_index" do
+    let(:response) do
+      html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+      Nokogiri::HTML(html)
+    end
+
+    it "returns 36 items" do
+      items = spider.send(:parse_index, response, url: store_url)
+      expect(items.length).to eq(36)
+    end
+  end
+
+  describe "#parse_index_node" do
+    context "with a regular index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_regular.html") }
+
+      let(:expected) do
+        {
+          url: "https://cartonazo.com/producto/gestos/",
+          title: "Gestos",
+          price: 22_990,
+          stock: true,
+          image_url: "https://cartonazo.com/wp-content/uploads/2025/06/gestos_cartonazo.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with a discounted price index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_discounted.html") }
+
+      let(:expected) do
+        {
+          url: "https://cartonazo.com/producto/celestia/",
+          title: "Celestia",
+          price: 26_350,
+          stock: true,
+          image_url: "https://cartonazo.com/wp-content/uploads/2025/03/Celestia_cartonazo.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+  end
+
+  describe "#next_page_url" do
+    context "with an index page that has a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has a next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = "https://cartonazo.com/categoria-producto/juego-de-mesa/page/2/"
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context "with an index page that hasn't a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_false.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has no next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = nil
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+end
