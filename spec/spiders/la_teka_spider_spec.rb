f814a96eb0feb6fe1f1de113a3561ed9f0512be4
nicolas <nicordenes@gmail.comm>
2025-06-17 11:35:26 -0400

    test(LaTekaSpider): add tests

---
+++ spec/spiders/la_teka_spider_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require "spider_helper"
+
+RSpec.describe LaTekaSpider do
+  let(:fixture_directory) { "la_teka" }
+  let(:spider) { described_class.new }
+  let(:store_url) { described_class.store[:url] }
+
+  it_behaves_like "a store spider"
+
+  describe "#parse_index" do
+    let(:response) do
+      html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+      Nokogiri::HTML(html)
+    end
+
+    it "returns 20 items" do
+      items = spider.send(:parse_index, response, url: store_url)
+      expect(items.length).to eq(20)
+    end
+  end
+
+  describe "#parse_index_node" do
+    context "with a regular index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_regular.html") }
+
+      let(:expected) do
+        {
+          url: "https://lateka.cl/products/wingspan-expansion-oceania",
+          title: "WINGSPAN: EXPANSIÓN OCEANÍA",
+          price: 35_990,
+          stock: true,
+          image_url: "https://lateka.cl/cdn/shop/products/3D-box.png"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with a discounted price index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_discounted.html") }
+
+      let(:expected) do
+        {
+          url: "https://lateka.cl/products/terraforming-mars-colonias-expansion",
+          title: "TERRAFORMING MARS: COLONIAS (Expansión)",
+          price: 26_900,
+          stock: true,
+          image_url: "https://lateka.cl/cdn/shop/products/FT_TMColonias-400x509.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with an out-of-stock index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_out_of_stock.html") }
+
+      let(:expected) do
+        {
+          url: "https://lateka.cl/products/brass-birmingham",
+          title: "BRASS: BIRMINGHAM",
+          price: 64_990,
+          stock: false,
+          image_url: "https://lateka.cl/cdn/shop/products/brass-birmingham-caja_1.jpg"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+  end
+
+  describe "#next_page_url" do
+    context "with an index page that has a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has a next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = "https://lateka.cl/collections/juegos-de-mesa?page=2"
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context "with an index page that hasn't a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_false.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has no next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = nil
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+end
