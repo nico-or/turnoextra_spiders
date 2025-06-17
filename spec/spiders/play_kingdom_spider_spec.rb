957b4e2903d42db330aa226b38af9fbc61e6e2b4
nicolas <nicordenes@gmail.com>
2025-06-17 12:35:15 -0400

    test(PlayKingdomSpider): add tests

---
+++ spec/spiders/play_kingdom_spider_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require "spider_helper"
+
+RSpec.describe PlayKingdomSpider do
+  let(:fixture_directory) { "play_kingdom" }
+  let(:spider) { described_class.new }
+  let(:store_url) { described_class.store[:url] }
+
+  it_behaves_like "a store spider"
+
+  describe "#parse_index" do
+    let(:response) do
+      html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+      Nokogiri::HTML(html)
+    end
+
+    it "returns 19 items" do
+      items = spider.send(:parse_index, response, url: store_url)
+      expect(items.length).to eq(19)
+    end
+  end
+
+  describe "#parse_index_node" do
+    context "with a regular index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_regular.html") }
+
+      let(:expected) do
+        {
+          url: "https://playkingdom.cl/la-tripulacion-en-busqueda-del-noveno-planeta",
+          title: "La Tripulacion - En busqueda del noveno planeta",
+          price: 14_990,
+          stock: true,
+          image_url: "https://cdnx.jumpseller.com/play-kingdom/image/30353600"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with a discounted price index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_discounted.html") }
+
+      let(:expected) do
+        {
+          url: "https://playkingdom.cl/lacrimosa",
+          title: "Lacrimosa",
+          price: 45_490,
+          stock: true,
+          image_url: "https://cdnx.jumpseller.com/play-kingdom/image/30152818"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+
+    context "with an out-of-stock index product node" do
+      let(:filename) { File.join("spec/fixtures", fixture_directory, "product_index_node_out_of_stock.html") }
+
+      let(:expected) do
+        {
+          url: "https://playkingdom.cl/mazescape-hipnos",
+          title: "Mazescape Hipnos",
+          price: 10_000,
+          stock: false,
+          image_url: "https://cdnx.jumpseller.com/play-kingdom/image/30354105"
+        }
+      end
+
+      it_behaves_like "a product node parser"
+    end
+  end
+
+  describe "#next_page_url" do
+    context "with an index page that has a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_true.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has a next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = "https://playkingdom.cl/juegos-de-mesa?page=2"
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context "with an index page that hasn't a next page link" do
+      let(:response) do
+        html = File.read(File.join("spec/fixtures", fixture_directory, "index_page_paginate_false.html"))
+        Nokogiri::HTML(html)
+      end
+
+      it "has no next page" do
+        actual = spider.next_page_url(response, store_url)
+        expected = nil
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+end
