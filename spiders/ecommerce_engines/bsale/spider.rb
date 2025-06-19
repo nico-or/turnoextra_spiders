5ef5904622fd7a5669e6a4e7ceefc4b81934fb3d
nicolas <nicordenes@gmail.comm>
2025-06-19 11:41:42 -0400

    refactor(Bsale::Spider): move selector logic up to ApplicationSpider

--- spiders/ecommerce_engines/bsale/spider.rb
+++ spiders/ecommerce_engines/bsale/spider.rb
@@ -4,11 +4,6 @@ module EcommerceEngines
   module Bsale
     # Base spider class for all stores build with Bsale
     class Spider < ApplicationSpider
-      def parse_index(response, url:, selector:, data: {})
-        listings = response.css(selector)
-        listings.map { |listing| parse_product_node(listing, url:) }
-      end
-
       def parse_product_node(node, url:)
         {
           url: get_url(node, url),
@@ -19,33 +14,12 @@ module EcommerceEngines
         }
       end
 
-      def next_page_url(response, url, selector)
-        next_page_node = response.at_css(selector)
-        return unless next_page_node
-
-        absolute_url(next_page_node[:href], base: url)
-      end
-
       private
 
-      def get_url(node, url)
-        rel_url = node.at_css("a")[:href]
-        absolute_url(rel_url, base: url)
-      end
-
-      def get_price(node, selector)
-        price_node = node.at_css(selector)
-        return unless price_node
-
-        scan_int(price_node.text)
-      end
-
-      def in_stock?(node, selector)
-        node.at_css(selector).nil?
-      end
-
-      def get_image_url(node, attribute)
-        node.at_css("img").attr(attribute).then do |url|
+      def get_image_url(node)
+        tag = get_selector(:image_tag)
+        attr = get_selector(:image_attr)
+        node.at_css(tag).attr(attr).then do |url|
           uri = URI.parse(url)
           uri.query = nil
           uri.to_s
