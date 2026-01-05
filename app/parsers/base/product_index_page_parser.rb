bfcfb52e96911b73c300cda1b87017b951cc99b5
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-05 20:07:27 -0300

    feat(Parsers): add Base::ProductIndexPageParser class

---
+++ app/parsers/base/product_index_page_parser.rb
@@ -0,0 +1,26 @@
+module Base
+  class ProductIndexPageParser
+    attr_reader :node, :base_url, :selectors
+
+    def initialize(node, base_url:, selectors: {})
+      @node = node
+      @base_url = base_url
+      @selectors = default_selectors.merge(selectors)
+    end
+
+    def default_selectors
+      {}
+    end
+
+    def product_nodes
+      node.css(selectors[:index_product])
+    end
+
+    def next_page_url
+      next_page_node = node.at_css(selectors[:next_page])
+      return unless next_page_node
+
+      Helpers.absolute_url(next_page_node[:href], base_url:)
+    end
+  end
+end
---
+++ spec/parsers/base/product_index_page_parser_spec.rb
@@ -0,0 +1,48 @@
+require "support/parsers_helper"
+
+RSpec.describe Base::ProductIndexPageParser do
+  let(:html) do
+    <<~HTML
+      <html>
+        <ul id='products'>
+          <li class='product'>product 1</li>
+          <li class='product'>product 2</li>
+          <li>not a product</li>
+        </ul>
+        <nav class='pagination'>
+          <li><a>page 1</a></li>
+          <li><a rel='next' href='/products/?page=2'>page 2</a></li>
+        </nav>
+      </html>
+    HTML
+  end
+
+  let(:selectors) do
+    {
+      index_product: "ul#products li.product",
+      next_page: "nav.pagination a[rel=next]"
+    }
+  end
+
+  let(:base_url) { "https://www.example.com" }
+
+  let(:subject) do
+    node = Nokogiri::HTML5.parse(html)
+    described_class.new(node, base_url:, selectors:)
+  end
+
+  describe "#product_nodes" do
+    it "returns 2 nodes" do
+      result = subject.product_nodes
+      expect(result.length).to eq(2)
+    end
+  end
+
+  describe "#next_page_url" do
+    it "returns the correct url" do
+      expected = "https://www.example.com/products/?page=2"
+      result = subject.next_page_url
+      expect(result).to eq(expected)
+    end
+  end
+end
