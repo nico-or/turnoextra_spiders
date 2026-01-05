f0ea26f37d56bb05cd27fcdba57db225f6024502
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-05 20:07:27 -0300

    refactor(SolettaSpider): use Soletta::ProductIndexPageParser

--- spiders/soletta_spider.rb
+++ spiders/soletta_spider.rb
@@ -9,18 +9,12 @@ class SolettaSpider < EcommerceEngines::Bsale::Spider
   }
   @start_urls = ["https://www.soletta.cl/collection/todos"]
 
-  selector :index_product, "div.bs-product"
-  selector :next_page, "ul.pagination li:last-child a.page-link[href]"
+  @index_parser_factory = ParserFactory.new(
+    Stores::Soletta::ProductIndexPageParser
+  )
+
   selector :title, "h2"
   selector :stock, "div.bs-stock"
   selector :price, "div.bs-product-final-price"
   selector :image_attr, "data-src"
-
-  def next_page_url(response, url)
-    selector = get_selector(:next_page)
-    next_page = response.at_css(selector)
-    return if next_page.nil? || next_page[:href] == "#"
-
-    absolute_url(next_page[:href], base: url)
-  end
 end
