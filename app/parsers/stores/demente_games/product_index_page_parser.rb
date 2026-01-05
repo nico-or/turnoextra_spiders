6adfb6b311fa420dec324410ba3067be2b8e26e0
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-05 20:07:27 -0300

    feat: add Stores::DementeGames::ProductIndexPageParser class

---
+++ app/parsers/stores/demente_games/product_index_page_parser.rb
@@ -0,0 +1,13 @@
+module Stores
+  module DementeGames
+    class ProductIndexPageParser < EcommerceEngines::Prestashop::ProductIndexPageParser
+      def next_page_url
+        next_page = node.at_css(selectors[:next_page])
+        return if next_page.nil?
+        return if next_page.classes.include?("disabled")
+
+        Helpers.absolute_url(next_page[:href], base_url:)
+      end
+    end
+  end
+end
