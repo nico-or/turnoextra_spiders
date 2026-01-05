dae4089f228299c10fc568c5ba83fb3be63cf6f7
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-05 20:07:27 -0300

    refactor(DivertiSpider): add index_page_parser_config

--- spiders/diverti_spider.rb
+++ spiders/diverti_spider.rb
@@ -10,13 +10,9 @@ class DivertiSpider < EcommerceEngines::PrestaShop::Spider
   @start_urls = ["https://www.diverti.cl/juegos-de-mesa-16?en-stock=1"]
   @config = {}
 
-  def next_page_url(response, url)
-    # This store doesn't disable the next page link on the last pagination result
-    next_page_node = response.at_css("nav.pagination li a[rel=next]")
-    return if next_page_node.nil? || next_page_node.classes.include?("disabled")
-
-    absolute_url(next_page_node[:href], base: url)
-  end
+  @index_parser_factory = ParserFactory.new(
+    Stores::Diverti::ProductIndexPageParser
+  )
 
   private
 
