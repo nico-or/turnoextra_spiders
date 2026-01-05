9fba01080a10718cab0189927bf4b0597df65324
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-05 20:07:27 -0300

    feat: add Helpers module

---
+++ app/parsers/helpers.rb
@@ -0,0 +1,10 @@
+module Helpers
+  def self.absolute_url(rel_url, base_url:)
+    uri = Addressable::URI.parse(rel_url).normalize
+    URI.join(base_url, uri).to_s
+  end
+
+  def self.scan_int(string)
+    string.scan(/\d/).join.to_i
+  end
+end
