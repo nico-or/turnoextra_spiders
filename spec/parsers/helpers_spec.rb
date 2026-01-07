1916843183770fc39da409869190ed027152dddd
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-07 15:18:30 -0300

    test: add parsers/helper_spec file

--- spec/parsers/helpers_spec.rb
+++ spec/parsers/helpers_spec.rb
@@ -1,21 +1,16 @@
 # frozen_string_literal: true
 
-require "spider_helper"
+require 'support/parsers_helper'
 
-RSpec.describe ApplicationSpider do
-  let(:spider) { described_class.new }
+RSpec.describe Helpers do
   let(:base_url) { "https://example.com/" }
 
-  before do
-    described_class.selector(:url, "a")
-  end
-
-  describe "#absolute_url" do
+  describe ".absolute_url" do
     context "when href is already percent-encoded" do
       let(:href) { "/path/to/product-%C2%BA-name" }
 
       it "does not double-encode the URL" do
-        result = spider.send(:absolute_url, href, base: base_url)
+        result = described_class.absolute_url(href, base_url:)
         expect(result).to eq("https://example.com/path/to/product-%C2%BA-name")
       end
     end
