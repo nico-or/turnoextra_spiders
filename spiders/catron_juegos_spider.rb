2f9856f8c96fb4db75d458bbbf03e81c964df443
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-08 11:50:45 -0300

    refactor(CatronJuegosSpider): use Jumpseller::ProductCardParser

--- spiders/catron_juegos_spider.rb
+++ spiders/catron_juegos_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Catron Juegos store spider
-class CatronJuegosSpider < EcommerceEngines::Jumpseller::Spider
+class CatronJuegosSpider < ApplicationSpider
   @name = "catron_juegos_spider"
   @store = {
     name: "Catron Juegos",
@@ -13,5 +13,10 @@ class CatronJuegosSpider < EcommerceEngines::Jumpseller::Spider
     EcommerceEngines::Jumpseller::ProductIndexPageParser
   )
 
-  selector :stock, "div.product-block__actions > a"
+  @product_parser_factory = ParserFactory.new(
+    EcommerceEngines::Jumpseller::ProductCardParser,
+    selectors: {
+      stock: "div.product-block__actions > a"
+    }
+  )
 end
