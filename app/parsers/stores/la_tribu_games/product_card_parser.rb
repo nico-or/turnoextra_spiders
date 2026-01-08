13c8f478b73d2a5eb415045ffae662d54e2c29c1
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-08 12:54:59 -0300

    refactor(LaTribuGamesSpider): use LaTribuGames::ProductCardParser

---
+++ app/parsers/stores/la_tribu_games/product_card_parser.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Stores
+  module LaTribuGames
+    class ProductCardParser < EcommerceEngines::Jumpseller::ProductCardParser
+      # TODO: remove explicit super.merge call
+      def default_selectors
+        super.merge(
+          {
+            stock: "a.not-available",
+            price: ".product-block-normal, .product-block-list"
+          }
+        )
+      end
+
+      def title
+        node.at_css("img")[:alt].strip
+      end
+    end
+  end
+end
--- spiders/la_tribu_games_spider.rb
+++ spiders/la_tribu_games_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # La Tribu Games store spider
-class LaTribuGamesSpider < EcommerceEngines::Jumpseller::Spider
+class LaTribuGamesSpider < ApplicationSpider
   @name = "la_tribu_games_spider"
   @store = {
     name: "La Tribu Games",
@@ -16,26 +16,7 @@ class LaTribuGamesSpider < EcommerceEngines::Jumpseller::Spider
     }
   )
 
-  selector :stock, "a.not-available"
-
-  private
-
-  def get_title(node)
-    node.at_css("img")[:alt].strip
-  end
-
-  def regular_price(node)
-    node.at_css("div.list-price span.product-block-list")
-  end
-
-  def discount_price(node)
-    node.at_css("div.list-price span.product-block-normal")
-  end
-
-  def get_price(node)
-    price_node = discount_price(node) || regular_price(node)
-    return unless price_node
-
-    scan_int(price_node.text)
-  end
+  @product_parser_factory = ParserFactory.new(
+    Stores::LaTribuGames::ProductCardParser
+  )
 end
