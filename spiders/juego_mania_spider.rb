88a253701dd9a00ad979765d36f1e520474d5469
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-08 12:05:35 -0300

    refactor(JuegoManiaSpider): use Jumpseller::ProductCardParser

--- spiders/juego_mania_spider.rb
+++ spiders/juego_mania_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Juego Mania store spider
-class JuegoManiaSpider < EcommerceEngines::Jumpseller::Spider
+class JuegoManiaSpider < ApplicationSpider
   @name = "juego_mania_spider"
   @store = {
     name: "Juego Mania",
@@ -13,25 +13,13 @@ class JuegoManiaSpider < EcommerceEngines::Jumpseller::Spider
     EcommerceEngines::Jumpseller::ProductIndexPageParser
   )
 
-  selector :stock, "div.product-block__label--status"
-
-  private
-
-  def regular_price(node)
-    node.at_css("div.product-block__price--new, div.product-block__price:last-child")
-  end
-
-  def discount_price(node)
-    price_node = node.at_css("div.product-block__price span:first-child")
-    return unless price_node
-
-    price_node
-  end
-
-  def get_price(node)
-    price_node = discount_price(node) || regular_price(node)
-    return unless price_node
-
-    scan_int(price_node.text)
-  end
+  @product_parser_factory = ParserFactory.new(
+    EcommerceEngines::Jumpseller::ProductCardParser,
+    selectors: {
+      stock: "div.product-block__label--status",
+      price: "div.product-block__price--new,
+      div.product-block__price:last-child,
+      div.product-block__price span:first-child"
+    }
+  )
 end
