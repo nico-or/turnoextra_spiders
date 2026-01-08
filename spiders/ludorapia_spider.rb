9be7f31ec49245abae0121bd2ed4978b302a9a44
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-08 12:59:06 -0300

    refactor(LudorapiaSpider): use Jumpseller::ProductCardParser

--- spiders/ludorapia_spider.rb
+++ spiders/ludorapia_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Ludorapia store spider
-class LudorapiaSpider < EcommerceEngines::Jumpseller::Spider
+class LudorapiaSpider < ApplicationSpider
   @name = "ludorapia_spider"
   @store = {
     name: "Ludorapia",
@@ -13,23 +13,7 @@ class LudorapiaSpider < EcommerceEngines::Jumpseller::Spider
     EcommerceEngines::Jumpseller::ProductIndexPageParser
   )
 
-  private
-
-  def regular_price(node)
-    node.at_css("div.product-block__price")
-  end
-
-  def discount_price(node)
-    price_node = node.at_css("div.product-block__price span:first-child")
-    return unless price_node
-
-    price_node
-  end
-
-  def get_price(node)
-    price_node = discount_price(node) || regular_price(node)
-    return unless price_node
-
-    scan_int(price_node.text)
-  end
+  @product_parser_factory = ParserFactory.new(
+    EcommerceEngines::Jumpseller::ProductCardParser
+  )
 end
