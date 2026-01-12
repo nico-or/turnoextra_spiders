f78fb9b451e3cd8ed96c9e97c85f8d6ae1bf58d3
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-12 15:50:03 -0300

    refactor(Shopify::Spider): remove unused methods

--- spiders/ecommerce_engines/shopify/spider.rb
+++ spiders/ecommerce_engines/shopify/spider.rb
@@ -9,50 +9,6 @@ module EcommerceEngines
           delay: 4..6
         }
       }
-
-      selector :url, "a"
-      selector :image_tag, "img"
-      selector :image_attr, "src"
-
-      def get_image_url(node, _url)
-        image_node = image_node(node)
-        url = image_url(image_node)
-        format_image_url(url)
-      rescue NoMethodError
-        nil
-      end
-
-      def image_node(node)
-        selector = get_selector(:image_tag)
-        node.at_css(selector)
-      end
-
-      def image_url(node)
-        selector = get_selector(:image_attr)
-        url = extract_image_url(node)
-        case selector
-        when "srcset"
-          url.split[-2]
-        else
-          url
-        end
-      end
-
-      def extract_image_url(node)
-        return unless node
-
-        selector = get_selector(:image_attr)
-        node.attr(selector)
-      end
-
-      def format_image_url(url)
-        uri = URI.parse(url)
-        uri.scheme = "https"
-        uri.query = nil
-        uri.to_s
-      rescue URI::InvalidURIError
-        nil
-      end
     end
   end
 end
