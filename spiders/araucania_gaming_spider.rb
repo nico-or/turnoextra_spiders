44cfb4418dd18c399df007c4f425c45b6ac39a6a
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-28 17:06:33 -0300

    refactor(AraucaniaGamingSpider): use WooCommerce::ProductCardParser

--- spiders/araucania_gaming_spider.rb
+++ spiders/araucania_gaming_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Araucania Gaming store spider
-class AraucaniaGamingSpider < EcommerceEngines::WooCommerce::Spider
+class AraucaniaGamingSpider < ApplicationSpider
   @name = "araucania_gaming_spider"
   @store = {
     name: "Araucania Gaming",
@@ -13,14 +13,11 @@ class AraucaniaGamingSpider < EcommerceEngines::WooCommerce::Spider
     EcommerceEngines::WooCommerce::ProductIndexPageParser
   )
 
-  selector :title, "h3"
-  selector :url, "h3 a"
-
-  private
-
-  def get_image_url(node, _url)
-    # NOTE: this stores mutates the HTML after loading
-    # what you see in the browser is not what mechanize gets
-    node.at_css("img")["data-srcset"].split(",").last.split.first
-  end
+  @product_parser_factory = ParserFactory.new(
+    EcommerceEngines::WooCommerce::ProductCardParser,
+    selectors: {
+      url: "h3 a",
+      img_sized_attr: "data-src"
+    }
+  )
 end
