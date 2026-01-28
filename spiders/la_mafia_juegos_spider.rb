16674491bc4da9988fd72f16862d19820a79f8bc
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-28 17:41:08 -0300

    refactor(LaMafiaJuegosSpider): use WooCommerce::ProductCardParser

--- spiders/la_mafia_juegos_spider.rb
+++ spiders/la_mafia_juegos_spider.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # La Mafia Juegos store spider
-class LaMafiaJuegosSpider < EcommerceEngines::WooCommerce::Spider
+class LaMafiaJuegosSpider < ApplicationSpider
   @name = "la_mafia_juegos_spider"
   @store = {
     name: "La Mafia Juegos",
@@ -13,10 +13,15 @@ class LaMafiaJuegosSpider < EcommerceEngines::WooCommerce::Spider
     EcommerceEngines::WooCommerce::ProductIndexPageParser
   )
 
-  image_url_strategy(:sized)
+  @product_parser_factory = ParserFactory.new(
+    EcommerceEngines::WooCommerce::ProductCardParser,
+    selectors: {
+      price: "span.price span.amount"
+    }
+  )
 
-  def get_price(node)
-    price_node = node.css("span.price span.amount").last
-    scan_int(price_node.text) if price_node
-  end
+  # def get_price(node)
+  #   price_node = node.css("span.price span.amount").last
+  #   scan_int(price_node.text) if price_node
+  # end
 end
