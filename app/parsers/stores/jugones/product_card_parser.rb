20b2e4e6b10ba3570444bf1893ee211c72ccacb1
Nicolás Órdenes <nicordenes@gmail.com>
2026-01-30 10:38:32 -0300

    feat: add Jugones::ProductCardParser class

---
+++ app/parsers/stores/jugones/product_card_parser.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Stores
+  module Jugones
+    class ProductCardParser < Base::ProductParser
+      def default_selectors
+        {
+          title: "a.modelo",
+          url: "a.modelo",
+          stock: "a.precio.reserva"
+        }
+      end
+
+      def price
+        Helpers.scan_int(price_node&.text)
+      end
+
+      def image_url
+        return unless image_rel_url
+
+        Helpers.absolute_url(image_rel_url, base_url:)
+      end
+
+      private
+
+      def regular_price
+        node.at_css("a.precio")&.children&.[](0)
+      end
+
+      def discount_price
+        node.at_css("a.precio.oferta")&.children&.[](2)
+      end
+
+      def price_node
+        discount_price || regular_price
+      end
+
+      def image_node
+        node.at_css("img[src]")
+      end
+
+      def image_rel_url
+        # Example: https://www.jugones.cl/img/cache/140x140_100_172828401616969534121681488307wow1.jpg
+        image_node&.attr("src")&.sub(%r{cache/\d+x\d+_\d+_}, "productos/")
+      end
+    end
+  end
+end
